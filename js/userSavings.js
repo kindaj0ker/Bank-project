ea589039a147c598b60ce5a0c672d224246a8688
kindaj0ker <zherebiateva.daria@gmail.com>
2023-01-09 21:23:25 +0400

    fbdsn

--- js/userSavings.js
+++ js/userSavings.js
@@ -28,25 +28,25 @@ function sortSavings() {
 let showAllSavBtn, showAllSavText;
 const categoriesBlock = document.querySelector(".categories-block");
 const categoriesTypes = document.querySelector(".all-savings--categories");
-function createSavingsType(savings) {
-  savings.forEach((s) => {
-    const htmlSaving = `<div class="category">
-        <img class="saving-category__img" id="${s}" src="../img/${s}-saving.png"/>
-        <p class="saving-category__name">${s.replace("-", " ")}</p>
-      </div>`;
-    categoriesTypes.insertAdjacentHTML("afterbegin", htmlSaving);
-  });
-  const html = `
-    <div class="show-all-sav--btn toggle-all--sav">
-      <h4 class="show-sav--text">Show all savings</h4>
-    </div>`;
-  categoriesBlock.insertAdjacentHTML("beforeend", html);
-  showAllSavBtn = document.querySelector(".show-all-sav--btn");
-  showAllSavText = document.querySelector(".show-sav--text");
-  showAllSavBtn.addEventListener("click", function () {
-    showAllSavings();
-  });
-}
+// function createSavingsType(savingsTypes) {
+//   savingsTypes.forEach((s) => {
+//     const htmlSaving = `<div class="category">
+//         <img class="saving-category__img" id="${s}" src="../img/${s}-saving.png"/>
+//         <p class="saving-category__name">${s.replace("-", " ")}</p>
+//       </div>`;
+//     categoriesTypes.insertAdjacentHTML("afterbegin", htmlSaving);
+//   });
+//   const html = `
+//     <div class="show-all-sav--btn toggle-all--sav">
+//       <h4 class="show-sav--text">Show all savings</h4>
+//     </div>`;
+//   categoriesBlock.insertAdjacentHTML("beforeend", html);
+//   showAllSavBtn = document.querySelector(".show-all-sav--btn");
+//   showAllSavText = document.querySelector(".show-sav--text");
+//   showAllSavBtn.addEventListener("click", function () {
+//     showAllSavings();
+//   });
+// }
 
 //Show total balance
 export function showTotalSavingsBalance() {
@@ -61,12 +61,19 @@ export function showTotalSavingsBalance() {
 
 //Show savings
 const savingsBlock = document.querySelector(".savings-block");
-function showSavings() {
-  const savingsTypes = new Set();
-  sortSavings().forEach((saving) => {
+
+//Show user savings types
+function showUserSavTypes() {
+  let savingsTypes = new Set();
+  return sortSavings().forEach((saving) => {
     const type = saving.savingType;
     savingsTypes.add(type);
   });
+}
+const g = showUserSavTypes();
+console.log(g);
+function showSavings() {
+  const savingsTypes = showUserSavTypes();
   createSavingsType(savingsTypes);
   showTotalSavingsBalance();
 }
@@ -95,7 +102,6 @@ function revealSavings(e) {
   savingsRevealingBlock.innerHTML = "";
 
   //Show balance
-
   let balance = 0;
   let currencySaving = "";
   sortSavings(savings).forEach((s) => {
@@ -120,8 +126,8 @@ savingTypeBtn.forEach((btn) => btn.addEventListener("click", revealSavings));
 //Manage savings
 const manageSavBtn = document.querySelector(".manage-sav--btn");
 const manageTypes = document.querySelectorAll(".manage-sav");
-const createSavBtn = document.querySelector(".create-sav--btn");
-const transferSavBtn = document.querySelector(".transfer-sav--btn");
+const createSavBtn = document.querySelector(".create-sav--btn h4");
+const transferSavBtn = document.querySelector(".transfer-sav--btn h4");
 const manageSavBlock = document.querySelector(".manage-sav--wrapper");
 const createSavBlock = document.querySelector(".create-sav--block");
 const transferSavBlock = document.querySelector(".transfer-sav--block");
@@ -130,15 +136,45 @@ manageSavBtn.addEventListener("click", function () {
   manageTypes.forEach((m) => m.classList.toggle("hidden"));
 });
 
-//Create/transfer new saving function
+//Create/transfer new saving
 manageTypes.forEach((t) => {
   t.addEventListener("click", function (e) {
-    const btnType = `${
-      e.target.closest(".manage-sav").classList[0].split("-")[0]
-    }SavBlock`;
-    console.log(btnType);
-    btnType.classList.remove("hidden");
-    manageSavBlock.classList.add("hidden");
-    categoriesBlock.remove();
+    if (e.target === createSavBtn) {
+      categoriesBlock.remove();
+      manageSavBlock.classList.add("hidden");
+      createSavBlock.classList.remove("hidden");
+    }
+    if (e.target === transferSavBtn) {
+      categoriesBlock.classList.remove("hidden");
+      manageSavBlock.classList.add("hidden");
+      transferSavBlock.classList.remove("hidden");
+    }
   });
 });
+
+// Create new saving
+const nextNewSavBtn = document.querySelector(".create-sav--btn__continue");
+const cancelNewSavBtn = document.querySelector(".create-sav--btn__cancel");
+const returnNewSavBtn = document.querySelector(".create-sav--btn__return");
+const addNewSavBtn = document.querySelector(".create-sav--btn__add");
+const inputNewSavAmount = document.querySelector(".inputField_new_sav");
+
+//Cancel new saving creation
+cancelNewSavBtn.addEventListener("click", function () {
+  manageTypes.forEach((t) => t.classList.add("hidden"));
+  createSavBlock.classList.add("hidden");
+  categoriesBlock.classList.remove("hidden");
+  manageSavBlock.classList.remove("hidden");
+  createSavingsType(savings);
+});
+
+//Go back in saving creation
+
+//Next in saving creation
+
+//Check saving amount input field
+// function checkNewSavAmount() {
+//   const insertedAmount = inputNewSavAmount.value;
+// }
+
+//Finish saving creation
